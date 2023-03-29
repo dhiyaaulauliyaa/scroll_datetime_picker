b901776cc3367248efb9f0068a0664ac43e2bd92
Dhiyaaul Auliyaa (Ado) <do.nvcbl@gmail.com>
2023-03-29 21:33:58 +0700

    feature: implement ScrollTypeListener

--- lib/src/widget/picker_widget.dart
+++ lib/src/widget/picker_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:scroll_datetime_picker/src/widget/scroll_type_listener.dart';
 
 class PickerWidget extends StatelessWidget {
-  const PickerWidget({
+  PickerWidget({
     super.key,
     required this.itemCount,
     required this.itemExtent,
@@ -20,23 +21,29 @@ class PickerWidget extends StatelessWidget {
   final ScrollController controller;
   final Widget Function(int index) builder;
 
+  final _isProgrammaticScroll = ValueNotifier<bool>(false);
+
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollNotification>(
-      onNotification: _onNotification,
-      child: ListWheelScrollView.useDelegate(
-        controller: controller,
-        itemExtent: itemExtent,
-        perspective: 0.001,
-        childDelegate: ListWheelChildBuilderDelegate(
-          childCount: infiniteScroll ? null : itemCount,
-          builder: (context, index) {
-            return Container(
-              height: itemExtent,
-              alignment: Alignment.center,
-              child: builder.call(index),
-            );
-          },
+    return ScrollTypeListener(
+      onScroll: (isProgrammaticScroll) =>
+          _isProgrammaticScroll.value = isProgrammaticScroll,
+      child: NotificationListener<ScrollNotification>(
+        onNotification: _onNotification,
+        child: ListWheelScrollView.useDelegate(
+          controller: controller,
+          itemExtent: itemExtent,
+          perspective: 0.001,
+          childDelegate: ListWheelChildBuilderDelegate(
+            childCount: infiniteScroll ? null : itemCount,
+            builder: (context, index) {
+              return Container(
+                height: itemExtent,
+                alignment: Alignment.center,
+                child: builder.call(index),
+              );
+            },
+          ),
         ),
       ),
     );
@@ -44,7 +51,9 @@ class PickerWidget extends StatelessWidget {
 
   bool _onNotification(ScrollNotification notification) {
     if (!controller.hasClients) return true;
+    if (_isProgrammaticScroll.value) return true;
 
+    /* Handle overshoot */
     if (notification is ScrollEndNotification) {
       final overshoot = controller.offset % itemExtent;
       final lowestExtent = controller.offset - overshoot;
@@ -52,16 +61,23 @@ class PickerWidget extends StatelessWidget {
       final endExtent =
           overshoot > midExtent ? lowestExtent + itemExtent : lowestExtent;
 
-      Future.delayed(Duration.zero, () {
+      Future.delayed(Duration.zero, () async {
+        /* Return if scrollView is still scrolling */
+        if (controller.position.isScrollingNotifier.value) return true;
+
         if (controller.hasClients) {
-          controller.animateTo(
+          final allowChange = !_isProgrammaticScroll.value;
+          final rowIndex = (endExtent / itemExtent).floor() % itemCount;
+
+          await controller.animateTo(
             endExtent,
             duration: const Duration(milliseconds: 500),
             curve: Curves.bounceOut,
           );
 
-          final rowIndex = (endExtent / itemExtent).floor() % itemCount;
-          onChange.call(rowIndex);
+          if (allowChange) {
+            onChange.call(rowIndex);
+          }
         }
       });
     }
