ecaa3f8a9a1cb83ed308c1543b5fa1094f49001c
Dhiyaaul Auliyaa (Ado) <do.nvcbl@gmail.com>
2023-09-25 11:30:20 +0700

    feat(src): :memo: add doc comment to DateTimePickerStyle

--- lib/src/entities/date_time_picker_style.dart
+++ lib/src/entities/date_time_picker_style.dart
@@ -1,5 +1,5 @@
 part of '../scroll_date_time_picker.dart';
-
+/// Set styles for the DateTime picker widget
 class DateTimePickerStyle {
   DateTimePickerStyle({
     this.activeStyle,
@@ -15,15 +15,60 @@ class DateTimePickerStyle {
       ),
     ),
   }) : assert(
-          centerDecoration?.color != null,
+          centerDecoration.color != null,
           'Center decoration should have background color',
         );
 
+  /// TextStyle to be used for the widget in the center of the picker.
+  /// Item that shown with this style will be set for the new value.
+  ///
+  /// Typically have bigger font size than `[inactiveStyle]`
+  /// to create maginfying effect
   final TextStyle? activeStyle;
+
+  /// TextStyle to be used for the widget outside the center of the picker.
+  /// Item that shown with this style will not be set for the new value.
+  ///
+  /// Typically have smaller font size than `[activeStyle]`
+  /// to create maginfying effect for the active value
   final TextStyle? inactiveStyle;
+
+  /// TextStyle to be used for the value that is not valid / can't be selected
+  /// such as:
+  ///   - Day 28/29-31 in the month of February,
+  ///   - Day 31 for the months that only has 30 days
+  ///   - Date before `[DateTimePickerOption.minDate]`
+  ///   - Date after `[DateTimePickerOption.maxDate]`
+  ///
+  /// The value shown in this style can't be selected. If selected, the picker
+  /// will automatically revert to its previous value
+  ///
+  /// Defaults to:
+  ///
+  ///       TextStyle(color: Colors.grey),
   final TextStyle disabledStyle;
 
+  /// Decoration to be used for the widget in the center of the picker.
+  /// Item that shown with this style will be set for the new value.
   final BoxDecoration? activeDecoration;
+
+  /// Decoration to be used for the widget outside the center of the picker.
+  /// Item that shown with this style won't be set for the new value.
   final BoxDecoration? inactiveDecoration;
-  final BoxDecoration? centerDecoration;
+
+  /// Decoration for the center area of the picker
+  /// 
+  /// Should have a background color to prevent overlap visibility
+  /// with inactive values
+  ///
+  /// Defaults to:
+  /// 
+  ///        BoxDecoration(
+  ///          color: Colors.white,
+  ///          border: Border(
+  ///            top: BorderSide(color: Colors.grey),
+  ///            bottom: BorderSide(color: Colors.grey),
+  ///          ),
+  ///        ),
+  final BoxDecoration centerDecoration;
 }
