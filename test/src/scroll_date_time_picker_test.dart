5b443b12841d48b591d0cb588c7c066c9e28ff4a
Dhiyaaul Auliyaa (Ado) <do.nvcbl@gmail.com>
2023-10-19 14:22:34 +0700

    test(src): :white_check_mark: add widget tests
    itemBuilder test & update DateOption test

--- test/src/scroll_date_time_picker_test.dart
+++ test/src/scroll_date_time_picker_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/intl.dart';
 import 'package:scroll_datetime_picker/src/scroll_date_time_picker.dart';
+import 'package:scroll_datetime_picker/src/widgets/picker_widget.dart';
 
 void main() {
   final testValues = [
@@ -54,6 +55,101 @@ void main() {
         },
       );
     }
+
+    testWidgets(
+      'when itemBuilder is not null, '
+      'ScrollDateTimePicker should show item based on itemBuilder',
+      (tester) async {
+        const itemBuilderColor = Color(0xFF4527A0);
+        const styleColor = Color(0xFFC8FC2E);
+
+        await tester.pumpWidget(
+          MaterialApp(
+            home: ScrollDateTimePicker(
+              key: const Key('ScrollDateTimePicker'),
+              itemExtent: 54,
+              onChange: (datetime) {},
+              dateOption: DateTimePickerOption(
+                dateFormat: DateFormat.yMMMMd(),
+                minDate: DateTime(2000, 6),
+                maxDate: DateTime(2026, 6),
+                initialDate: DateTime(2020),
+              ),
+              style: DateTimePickerStyle(
+                centerDecoration: const BoxDecoration(color: Colors.white),
+                activeStyle: const TextStyle(color: styleColor),
+              ),
+              itemBuilder: (
+                context,
+                pattern,
+                text,
+                isActive,
+                isDisabled,
+              ) =>
+                  Text(
+                text,
+                key: text == '2020' ? const ValueKey('2020') : null,
+                style: isDisabled
+                    ? const TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                      )
+                    : isActive
+                        ? const TextStyle(
+                            fontSize: 20,
+                            color: itemBuilderColor,
+                            fontWeight: FontWeight.w700,
+                          )
+                        : TextStyle(
+                            fontSize: 18,
+                            color: itemBuilderColor.withOpacity(0.7),
+                          ),
+              ),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle(const Duration(milliseconds: 200));
+
+        final text = tester.widget<Text>(
+          find.byKey(const ValueKey('2020')).last,
+        );
+
+        expect(text.style?.color == itemBuilderColor, true);
+        expect(text.style?.color != styleColor, true);
+      },
+    );
+
+    testWidgets(
+      'when dateFormat is changed, '
+      'ScrollDateTimePicker should adjust its controllers',
+      (tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: _TestPage(
+              format: DateFormat('EEEEyyMMMdd'),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle(const Duration(milliseconds: 200));
+
+        // Make sure initial value is true
+        expect(find.byType(PickerWidget), findsNWidgets(4));
+
+        // Increase DateFormat
+        await tester.tap(find.byKey(const ValueKey('Increase Pattern')));
+        await tester.pumpAndSettle();
+
+        // Make sure number of picker is true after DateFormat is increased
+        expect(find.byType(PickerWidget), findsNWidgets(8));
+
+        // Decrease DateFormat
+        await tester.tap(find.byKey(const ValueKey('Decrease Pattern')));
+        await tester.pumpAndSettle();
+
+        // Make sure number of picker is true after DateFormat is decreased
+        expect(find.byType(PickerWidget), findsNWidgets(7));
+      },
+    );
   });
 }
 
@@ -68,11 +164,39 @@ class _TestPage extends StatefulWidget {
 
 class _TestPageState extends State<_TestPage> {
   DateTime date = DateTime(2023, 2, 7, 10, 10, 10);
+  late DateFormat _format;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _format = widget.format;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: Column(
+        children: [
+          FloatingActionButton(
+            key: const ValueKey('Increase Pattern'),
+            onPressed: () {
+              setState(
+                () => _format = DateFormat('EEEEyMMMddhhmmssa'),
+              );
+            },
+          ),
+          FloatingActionButton(
+            key: const ValueKey('Decrease Pattern'),
+            onPressed: () {
+              setState(
+                () => _format = DateFormat('EEEEyMMMddHHmmss'),
+              );
+            },
+          ),
+        ],
+      ),
       body: ScrollDateTimePicker(
         key: const Key('ScrollDateTimePicker'),
         itemExtent: 54,
@@ -80,7 +204,7 @@ class _TestPageState extends State<_TestPage> {
           date = datetime;
         }),
         dateOption: DateTimePickerOption(
-          dateFormat: widget.format,
+          dateFormat: _format,
           minDate: DateTime(2000, 6),
           maxDate: DateTime(2026, 6),
           initialDate: date,
