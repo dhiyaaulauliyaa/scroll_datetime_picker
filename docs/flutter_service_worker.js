9cf10fca7b14d5073970415d7c479c325dbcb4cf
Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2025-03-06 14:07:51 +0700

    fix 🛠️: fix web file for example

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "6299e49bd99b86319db5e9392059811b",
-"/": "6299e49bd99b86319db5e9392059811b",
-"main.dart.js": "94a167ee6e4d98951699b40caf57d72e",
+"index.html": "9437d9b2f3c9cb6f0bff67c316296770",
+"/": "9437d9b2f3c9cb6f0bff67c316296770",
+"main.dart.js": "bede900783d064aa2f3e33c4b34479d4",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
